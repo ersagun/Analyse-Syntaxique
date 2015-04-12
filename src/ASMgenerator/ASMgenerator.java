f555ef680c8c445671bca05927fdd1e813ab453a
Fikoul <sfikoul@yahoo.fr>
2015-04-12 11:24:15 +0200

    ASMGenerator
    Ajout du package ASMgenerator et de de la classe ou seront implémentées
    les fonctions generated java

---
+++ src/ASMgenerator/ASMgenerator.java
@@ -0,0 +1,32 @@
+package ASMgenerator;
+
+import TDS.*;
+import AST.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ASMgenerator {
+
+	public TDS tds;
+	public Noeud node;
+	
+	
+	/*
+	 * CONSTRUCTORS
+	 */
+	public ASMgenerator(TDS tds, Noeud node) {
+		super();
+		this.tds = tds;
+		this.node = node;
+	}
+	
+	/*
+	 * METHODES
+	 */
+	
+	
+	
+	
+}
