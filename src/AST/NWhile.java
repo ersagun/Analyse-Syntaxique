a730e10978d70477b861c6aa56b626169dc41211
Ersagun <ersagunyalcintepe@gmail.com>
2015-04-14 12:34:45 +0200

    while supplementaire
    while sup

--- src/AST/NWhile.java
+++ src/AST/NWhile.java
@@ -4,12 +4,15 @@ import TDS.TDS;
 
 public class NWhile extends NoeudElement {
 
+	public NWhile() {
+		super("while");
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String afficherNoeud(TDS tds) {
 		// TODO Auto-generated method stub
+		return "Noeud while => #while";
 	}
 
 }
