64ee4e6ccfa60e64327f546278f6a3d79739b969
Ersagun <ersagunyalcintepe@gmail.com>
2015-04-25 12:43:11 +0200

    testgrammaire a voir (fikoul)

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		ParserCup parser;
 		try {
-			parser = new ParserCup(new AnalyseurLexical(new FileInputStream("./fichiers/06-local.miage")));
+			parser = new ParserCup(new AnalyseurLexical(new FileInputStream("./fichiers/05-expression.miage")));
 			parser.parse();
 			System.out.println("TDS :");
 			System.out.println(parser.afficheTDSetAST());
